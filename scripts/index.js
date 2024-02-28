c60d0d316d03855d72558f5370376036d21210a9
NIKHIL KAMBLE <153905674+nikhilkamble5@users.noreply.github.com>
2024-02-29 00:33:05 +0530

    project complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,55 +1,52 @@
-let baagItems;
+let bagItems;
+onLoad();
 
-onLoad(); 
-//web page load zhya wr ky load hoila pahije
-
-function onLoad(){
-    let  baagItemsStr=localStorage.getItem('bagItems');
-    baagItems =baagItemsStr ? JSON.parse(baagItemsStr):[];
-    displayItemsOnHomePage();
-displayBagIcon();
+function onLoad() {
+  let bagItemsStr = localStorage.getItem('bagItems');
+  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+  displayItemsOnHomePage();
+  displayBagIcon();
 }
 
-function addToBag(itemId){
-    baagItems.push(itemId)
-    localStorage.setItem('bagItems',JSON.stringify(baagItems));
-    displayBagIcon();
+function addToBag(itemId) {
+  bagItems.push(itemId);
+  localStorage.setItem('bagItems', JSON.stringify(bagItems));
+  displayBagIcon();
 }
 
-function displayBagIcon(){
-    let bagItemCountElement=document.querySelector('.bag-item-count');
-//bag icon select kela 0 asel tr bag text hiddne hoil add kelya vr disel vr method call keli
-    if(baagItems.length>0){
-        bagItemCountElement.style.visibility='visible';
-
-        bagItemCountElement.innerText=baagItems.length
-     }
-     else{
-        bagItemCountElement.style.visibility='hidden';
-     }
+function displayBagIcon() {
+  let bagItemCountElement = document.querySelector('.bag-item-count');
+  if (bagItems.length > 0) {
+    console.log('I am here');
+    bagItemCountElement.style.visibility = 'visible';
+    bagItemCountElement.innerText = bagItems.length;
+  } else {
+    bagItemCountElement.style.visibility = 'hidden';
+  }
 }
 
-function displayItemsOnHomePage(){let itemsContainerElement = document.querySelector('.items-container')//elemetn cha class select kela
-
-//konta pn item add kelya saglyana style apply zhali pahije 👇
-let innerHTML =''; //empty html dila
-items.forEach(item =>{
-    // for each loop laun data retrive karaycha tyala html apply hoil
-    innerHTML +=` <div class="item-container">
-    <img class="item-image" src="${item.image}" alt="item image">
-    <div class="rating">${item.rating.stars} ⭐ |${item.rating.count}</div>
-    <div class="componey-name">${item.company}</div>
-    <div class="item-name">${item.item_name}</div>
-    
-    
-    <div class="price">
-        <span class="current-price">Rs ${item.current_price}</span>
-        <span class="origonal-price">Rs ${item.origonal_price}</span>
-        <span class="discount">(${item.discount_percentage}% OFF)</span>
-    </div>
-    <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+function displayItemsOnHomePage() {
+  let itemsContainerElement = document.querySelector('.items-container');
+  if (!itemsContainerElement) {
+    return;
+  }
+  let innerHtml = '';
+  items.forEach(item => {
+    innerHtml += `
+    <div class="item-container">
+      <img class="item-image" src="${item.image}" alt="item image">
+      <div class="rating">
+          ${item.rating.stars} ⭐ | ${item.rating.count}
+      </div>
+      <div class="company-name">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="price">
+          <span class="current-price">Rs ${item.current_price}</span>
+          <span class="original-price">Rs ${item.original_price}</span>
+          <span class="discount">(${item.discount_percentage}% OFF)</span>
+      </div>
+      <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
     </div>`
-})
-
-itemsContainerElement.innerHTML = innerHTML;
+  });
+  itemsContainerElement.innerHTML = innerHtml;
 }
